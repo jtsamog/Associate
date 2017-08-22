488023650500515845f3b022a09058ec626fd0a2
hkimca <harrison.kim.ca@gmail.com>
2017-08-22 15:12:35 -0400

    connections view/detail view hooked up

--- Associate/ConnectionsTableViewController.swift
+++ Associate/ConnectionsTableViewController.swift
@@ -11,7 +11,6 @@ import Parse
 
 class ConnectionsTableViewController: UITableViewController {
     
-    
     var connectionsArray:[PFUser] = [PFUser]()
     
     //MARK: Cell and Id
@@ -20,30 +19,52 @@ class ConnectionsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
+    //MARK: Datasource/Delegate
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return connectionsArray.count
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "connection", for: indexPath) as! ConnectionTableViewCell
+        
+        let connection = connectionsArray[indexPath.row]
+        cell.userNameLabel.text = connection["fullname"] as? String
+        cell.userDescripLabel.text = connection["profession"] as? String
+        //cell.userPicImageView.image = connection.photo
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedConnection = connectionsArray[ (indexPath as NSIndexPath).row ]
+        self.performSegue(withIdentifier: "showDetail", sender: selectedConnection)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        if let inConnection = sender as? PFUser {
+            let navVC = segue.destination as? UINavigationController
+            let currentVC = navVC?.viewControllers.first as? ConnectionsTableViewController
+            
+            //currentVC?.connection = inConnection
+        }
+    }
+}
+
+extension ConnectionsTableViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        retrieveConnections()
+    }
+}
+
+private extension ConnectionsTableViewController {
     
     func retrieveConnections() {
         
@@ -59,66 +80,4 @@ class ConnectionsTableViewController: UITableViewController {
             print(self.connectionsArray)
         })
     }
-
-    
-    
-    
-    
-    
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- UserTableViewCell.swift
+++ UserTableViewCell.swift
@@ -35,6 +35,10 @@ class UserTableViewCell: UITableViewCell {
     @IBAction func connectTapped(_ sender: UIButton) {
         
         
+        //connectImageView.image = UIImage(named: "Checked")
+        //print("image change")
+        
+        
     }
 
 }
