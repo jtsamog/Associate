92f7a9dd45712b495e58b10740f6d332f2153d1a
hkimca <harrison.kim.ca@gmail.com>
2017-08-23 11:50:41 -0400

    Chat creator name/image complete

--- Associate/CurrentEventViewController.swift
+++ Associate/CurrentEventViewController.swift
@@ -40,6 +40,9 @@ class CurrentEventViewController: UIViewController, UITableViewDataSource, UITab
     var menuShowing = false
     
     var messagesArray:[String] = [String]()
+    var creatorsArray:[PFUser] = [PFUser]()
+    
+    var messagesObjArray:[PFObject] = [PFObject]()
     
     var joined = false
     
@@ -89,37 +92,24 @@ class CurrentEventViewController: UIViewController, UITableViewDataSource, UITab
     }
     
     //MARK: PrepareForSegue
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
         if(segue.identifier == "usersInEvent") {
-            
             let tvc: EventUsersTableViewController = (segue.destination as? EventUsersTableViewController)!
-                
-             tvc.usersInEventArray = usersArray
-            
-           
+            tvc.usersInEventArray = usersArray
         }
     }
     
     //MARK: Actions
     
     @IBAction func logoutTapped(_ sender: UIButton) {
-        
         PFUser.logOut()
-        
         _ = PFUser.current()
-        
         self.performSegue(withIdentifier: "logoutPOP", sender: nil)
     }
     
-    
-    
     @IBAction func joinTapped(_ sender: Any) {
-        
         let joinedEvent = event
         let member = PFUser.current()
-        
         let relation:PFRelation = joinedEvent!.relation(forKey: "membersInEvent")
         relation.add(member!)
         joinedEvent!.saveInBackground(block: { (success, error) -> Void in
@@ -130,20 +120,16 @@ class CurrentEventViewController: UIViewController, UITableViewDataSource, UITab
                 print(error!)
             }
         })
-        
         joinView.isHidden = true
         usersInEventBarButton.isEnabled = true
         joined = true
         backOrMenuBarButton.image = UIImage(named: "Table")
-        
     }
     
     @IBAction func sendButtonTapped(_ sender: UIButton) {
-        
         self.messageTextField.endEditing(true)
         self.messageTextField.isEnabled = false
         self.sendButton.isEnabled = false
-        
         let joinedEvent = event!
         let newMessage = EventMessage(msgText: messageTextField.text, creator: PFUser.current() as? EventUser, event: joinedEvent)
         newMessage.saveInBackground(block: { (success, error) -> Void in
@@ -165,14 +151,9 @@ class CurrentEventViewController: UIViewController, UITableViewDataSource, UITab
     }
   
     @IBAction func menuTapped(_ sender: UIBarButtonItem) {
-        
-        
         if (joined == false) {
-            
             self.dismiss(animated: true, completion: nil)
-            
         } else {
-        
         if(menuShowing) {
             leadingContraint.constant = -200
             UIView.animate(withDuration: 0.3, animations: {
@@ -187,15 +168,12 @@ class CurrentEventViewController: UIViewController, UITableViewDataSource, UITab
             })
         }
         menuShowing = !menuShowing
-    }
-        
+        }
     }
     
     @IBAction func leaveEventTapped(_ sender: UIButton) {
-        
         let joinedEvent = event
         let member = PFUser.current()
-        
         let relation:PFRelation = joinedEvent!.relation(forKey: "membersInEvent")
         relation.remove(member!)
         joinedEvent?.saveInBackground(block: { (success, error) -> Void in
@@ -212,16 +190,16 @@ class CurrentEventViewController: UIViewController, UITableViewDataSource, UITab
     
     //MARK: Retrievals
     func retrieveMessages() {
-        
         let joinedEvent = event!
         let query = PFQuery(className: "EventMessage")
         query.whereKey("event", equalTo: joinedEvent)
+        query.includeKey("creator")
         query.findObjectsInBackground(block: { (objects:[PFObject]?, error:Error?) -> Void in
-            self.messagesArray = [String]()
+            self.messagesObjArray = [PFObject]()
             for messageObject in objects! {
-                let messageText:String? = (messageObject as PFObject)["msgText"] as? String
-                if messageText != nil {
-                    self.messagesArray.append(messageText!)
+                if (messageObject["msgText"] != nil) && (messageObject["creator"] != nil) {
+                    self.messagesObjArray.append(messageObject)
+                    print(self.messagesObjArray)
                 }
             }
             DispatchQueue.main.async {
@@ -229,9 +207,9 @@ class CurrentEventViewController: UIViewController, UITableViewDataSource, UITab
                 self.activityIndicator.stopAnimating()
                 self.refreshControl.endRefreshing()
             }
-        })
+            })
     }
-    
+
     func retrieveUsers() {
         
         let joinedEvent = event!
@@ -250,12 +228,10 @@ class CurrentEventViewController: UIViewController, UITableViewDataSource, UITab
 
     //MARK: Textfield Delegate
     func tableViewTapped() {
-        
         self.messageTextField.endEditing(true)
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        
         UIView.animate(withDuration: 0.21, animations: {
             self.dockViewHeightConstaint.constant = 335
             self.view.layoutIfNeeded()
@@ -263,7 +239,6 @@ class CurrentEventViewController: UIViewController, UITableViewDataSource, UITab
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        
         UIView.animate(withDuration: 0.4, animations: {
             self.dockViewHeightConstaint.constant = 60
             self.view.layoutIfNeeded()
@@ -272,31 +247,41 @@ class CurrentEventViewController: UIViewController, UITableViewDataSource, UITab
     
     //MARK: Datasource/Delegate
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = self.messageTableView.dequeueReusableCell(withIdentifier: "post", for: indexPath)
-        cell.textLabel?.text = self.messagesArray[indexPath.row]
+        let cell: UserPostTableViewCell = messageTableView.dequeueReusableCell(withIdentifier: "post", for: indexPath) as! UserPostTableViewCell
+        let message = self.messagesObjArray[indexPath.row]
+        debugPrint(message)
+        let creator = message["creator"] as! PFUser
+        cell.messageLabel?.text = message["msgText"] as? String
+        cell.postNameLabel?.text = creator["fullname"] as? String
+        if let imageFile = creator["profileImage"] as? PFFile {
+            imageFile.getDataInBackground { data, error in
+                if let error = error {
+                    print( #line, error.localizedDescription)
+                    return
+                }
+                guard let data = data else {
+                    print(#line, "No Photo to retrieve")
+                    return
+                }
+                cell.postImageView.image = UIImage(data: data)
+            }
+        }
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return messagesArray.count
+        return messagesObjArray.count
     }
 }
 
-//MARK: UICode 
-
+//MARK: UICode
 private extension CurrentEventViewController {
-    
     func prettyUI() {
-        
         leaveEventButton.layer.backgroundColor = UIColor.red.cgColor
         leaveEventButton.layer.borderColor = UIColor.white.cgColor
         leaveEventButton.layer.borderWidth = 3
         leaveEventButton.layer.cornerRadius = 10
-        
         for button in blueButtons {
-
             button.layer.backgroundColor = UIColor.blue.cgColor
             button.layer.borderColor = UIColor.white.cgColor
             button.layer.borderWidth = 3
